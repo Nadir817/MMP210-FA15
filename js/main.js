44d86cfe4bbe8622665055d2e9835f092f5a13a8
Nadir817 <Nadir@dhcp-10-30-34-209.bmcc.cuny.edu>
2015-10-26 14:23:32 -0400

    work

--- js/main.js
+++ js/main.js
@@ -1,19 +1,15 @@
-//global variables
-var myWinWidth, myWinHeight; //size of the main part of the window
+var myWinWidth, myWinHeight;
 
 function setup(){
-    //create the canvas. put into a variable to associate with a element
     var cnv = createCanvas(800,3000);
     cnv.parent("windows");
 
-    //set the width and height of the window i will draw
     myWinHeight = 250;
     myWinWidth = 100;
 
-    var colWidth = 160; //distance between windows going across. should be bigger then myWinWidth
-    var rowHeight = 290; //distance btween windows going down. should be bigger than myWinHeight
+    var colWidth = 160;
+    var rowHeight = 290;
 
-    //these variables are used to keep track of where we will be adding each window in the for loop
     var x = 0;
     var y = 0;
 
@@ -25,43 +21,43 @@ function setup(){
             x = 0;
             y += rowHeight;
         }
-    }//end for loop
-}//end setup
+    }
+}
 
 function drawWindow(startX, startY){
-    // fill color
-    fill('purple');
-    //line color
-    stroke('black');
 
-    //main window
+    fill('purple');
+
+    stroke('silver');
+
+
     rect(startX, startY,myWinWidth,myWinHeight);
 
-    //position variables for adding window details
+
     var x1,x2,y1,y2;
 
-    //vertical line down the middle
-    x1 = startX+myWinWidth/2; //horizontal middle of window
-    y1 = startY; //top of window
-    x2 = startX+myWinWidth/2; //horizontal middle of window
-    y2 = startY + myWinHeight; // bottom of window
+
+    x1 = startX+myWinWidth/2;
+    y1 = startY;
+    x2 = startX+myWinWidth/2;
+    y2 = startY + myWinHeight;
     line(x1,y1,x2,y2 );
 
-    //horizontal line 1/3 (.333) of the way down
-    x1 = startX; // left side
-    y1 = startY+myWinHeight*.333; // 1/3 of the way down
-    x2 = startX+myWinWidth; //right side of window
-    y2 = y1; //top of window
+
+    x1 = startX;
+    y1 = startY+myWinHeight*.333;
+    x2 = startX+myWinWidth;
+    y2 = y1;
     line(x1,y1,x2,y2 );
 
-    //horizontal line 2/3 (.666) of the way down
-    x1 = startX; //left side of window
-    y1 = startY+myWinHeight*.666; // 2/3 of the way down
-    x2 = startX+myWinWidth; //right side of window
-    y2 = y1; // 2/3 of the way down
+
+    x1 = startX;
+    y1 = startY+myWinHeight*.666;
+    x2 = startX+myWinWidth;
+    y2 = y1;
     line(x1,y1,x2,y2 );
 
-    //add box at the bottom
+
     x1 = startX - 20;
     y1 = startY + myWinHeight;
     rect(x1,y1,myWinWidth+40, 20);
@@ -70,6 +66,6 @@ function drawWindow(startX, startY){
 
 
 
-}//end drawWindow
+}
 
 
